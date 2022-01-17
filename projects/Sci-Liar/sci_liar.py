c178d3b2a0e60dbe7f9979ad4746bb8847daa425
EnigmaCurry <ryan@enigmacurry.com>
2022-01-17 00:57:32 -0800

    sci-liar-7db6d

--- projects/Sci-Liar/sci_liar.py
+++ projects/Sci-Liar/sci_liar.py
@@ -6,15 +6,15 @@ from daw.chop import chop, sequence
 
 def seq_one():
     s = lambda x: [x*12, x*13] * 2 + [x*14, x*15] * 2
-    return [s(i) * 2 for i in range(8)]
+    return [s(i) * 2 for i in range(24)]
 
 def seq_two():
-    s = lambda x: [x*12, x*15] + [x*15, x*12] + [x*14, x*13] + [x*13, x*14]
-    return [s(i) * 2 for i in range(24)]
+    s = lambda x: [x*12, x*15] + [x*15, x*12] + [x,x+1,x+2,x+3] + [x*14, x*13] + [x*13, x*14]
+    return [s(i) * 2 for i in range(64)]
 
 def liar(audio_file, bpm, pattern_func):
     audio = load_audio(audio_file)
-    bars = 7/16
+    bars = 1/24
     slices = chop(audio, bpm, bars, fade_in=2, fade_out=2)
     new_slices = []
     for i,s in enumerate(slices):
@@ -29,11 +29,11 @@ def liar(audio_file, bpm, pattern_func):
 
     pat = pattern_func()
     seq = sequence(new_slices, [item for sublist in pat for item in sublist][8:])
-    seq = stretch(seq, 0.7)
+    seq = stretch(seq, 1.7)
     date = time.strftime("%y-%m-%d")
     rnd_str = str(uuid.uuid1())[:5]
     save_audio(seq, f"~/Music/{date}-sci-liar-{rnd_str}.wav")
     #play(seq)
 
 if __name__ == "__main__":
-    liar(audio_file="Liar.mp3", bpm=120, pattern_func=seq_one)
+    liar(audio_file="Liar.mp3", bpm=120, pattern_func=seq_two)
